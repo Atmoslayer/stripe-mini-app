4af0f2d982e26f3fa33fb6c7dcd2464d93c23a52
Atmoslayer <ddsomdim@gmail.com>
2023-12-25 17:12:08 +0300

    Set up DRF

--- stripe_service/settings.py
+++ stripe_service/settings.py
@@ -26,7 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'payment_api'
+    'payment_api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
